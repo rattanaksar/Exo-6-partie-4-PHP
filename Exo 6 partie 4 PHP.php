905574f2b8f33a0e8b3ead2ca650ba67f63d03f5
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-10 15:32:29 +0200

    Fichier PHP

---
+++ Exo 6 partie 4 PHP.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 5 : Les boucles</title>
+</head>
+<body>
+    <h1>Exo 6 : Les boucles</h1>
+    <h2>Exo 6</h2>
+    <p>
+    En allant de 20 à 0 avec un pas de 1, afficher le message C'est presque bon.
+    </p>
+    <p>
+        <?php
+        for($i = 20; $i >= 1; $i--) {
+            echo "presque bon". "<br>" ;
+        }        
+        ?>
+    </p>
+</body>
+</html>
